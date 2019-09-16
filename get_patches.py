fad842f84ec5d1f174ce41bfd81311b3303e8d0b
xzmystyle <xzmystyle@hotmail.com>
2019-09-16 19:36:42 +0800

    Add files via upload

---
+++ get_patches.py
@@ -0,0 +1,22 @@
+import py_wsi
+import py_wsi.imagepy_toolkit as tk
+import os
+
+file_dir = "./data/ONE/"
+db_location = "./data/Label_patches/0089/"
+xml_dir = file_dir
+patch_size = 256
+level = 16
+db_name = "patch_db"
+overlap = 0
+# All possible labels mapped to integer ids in order of increasing severity.
+label_map = {'Normal':0,'Viable Tumor':1,'Negative Pen':2}
+
+if not os.path.exists(db_location):
+    os.mkdir(db_location)
+
+turtle = py_wsi.Turtle(file_dir, db_location, db_name, storage_type = 'disk',xml_dir = xml_dir, label_map=label_map)
+
+print("Patch size:", patch_size)
+turtle.sample_and_store_patches(patch_size, level, overlap , load_xml=True, limit_bounds=True)
+
